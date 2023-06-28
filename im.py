0508b7fc79210abea8f6e61dd1b528424ea10693
asraful islam <asraful009@gmail.com>
2023-06-28 17:19:13 +0600

    font reeangle

--- im.py
+++ im.py
@@ -1,6 +1,6 @@
 from PIL import Image, ImageDraw, ImageFont
 import random
-
+from faker import Faker
 import os
 
 
@@ -12,25 +12,25 @@ def get_hand_writting_font():
         os.listdir(src_dir))
   ))  
   index = random.randint(0, len(font_files)-1)
-  print(font_files, index)
+  # print(font_files, index)
   return font_files[index]
 
 def draw_border(img):
   (w, h) = img.size
   boxs = [
-    [(10, 10), (w - 10, h - 10)],
-    [(25, 25), (w - 25, h - 25)]
+    [(5, 5), (w - 5, h - 5)],
+    [(10, 10), (w - 10, h - 10)]
   ]
   imgDraw = ImageDraw.Draw(img)
   hex_color = "#0E2954"
   color = tuple(int(hex_color[i:i+2], 16) for i in (1, 3, 5))
   for box in boxs:
-    imgDraw.rectangle(box, outline=color , width=5)
+    imgDraw.rectangle(box, outline=color , width=2)
 
   
 
 
-def generater_img(image_width = 400, image_height = 300):
+def generater_img(name, job, company, address, image_width = 400, image_height = 150):
   # Define the image size and color
   
   image_color = (0, 0, 0, 1)
@@ -39,12 +39,24 @@ def generater_img(image_width = 400, image_height = 300):
   draw_border(image)
   font_path = get_hand_writting_font()
   
-  font_size = 100
+  font_size = 20
   font = ImageFont.truetype(font_path, font_size)
   imgDraw = ImageDraw.Draw(image)
-  imgDraw.text((40, 40), "hi all hjghj", font=font, fill="#0E2954")
+  imgDraw.text((20, 20), name, font=font, fill="#0E2954")
+  font = ImageFont.truetype(font_path, font_size-4)
+  imgDraw.text((20, 42), job, font=font, fill="#0E2954")
+  font = ImageFont.truetype(font_path, font_size-8)
+  imgDraw.text((20, 54), company, font=font, fill="#0E2954")
+  imgDraw.text((20, 70), address, font=font, fill="#0E2954")
 
   # Save the image to a file
   image.save('generated_image.png')
 
-generater_img()
+def main():
+  fake = Faker()
+  name = fake.name()
+  address = "{0}, {1}-{2}".format(fake.street_address(), fake.city(), "3243")
+  generater_img(name=name, job=fake.job(), company=fake.company(), address=address)
+
+if __name__ == "__main__":
+  main()
